7ce62caa32ed1fe6e5a57ec53f10982d7ffb9183
Tripp Hopkins <tripphopkins13245@gmail.com>
2024-07-15 15:42:34 -0400

    added pointers

---
+++ Pointers_Explained.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+int main(){
+
+    //pointers = variables that store a memory address of another variable
+    //              sometimes it's easier to work with an address
+
+    // & address-of operator
+    // dereference operator
+
+    std::string name = "Tripp";
+    int age = 15;
+    std::string freePizzas[5] = {"P1", "P2", "P3", "P4", "P5"};
+
+    std::string *pName = &name;
+    int *pAge = &age;
+    std::string *pFreePizzas = freePizzas;
+
+    std::cout << *pName << '\n';
+    std::cout << *pAge << '\n';
+    std::cout << *freePizzas << '\n';
+
+    return 0;
+}
